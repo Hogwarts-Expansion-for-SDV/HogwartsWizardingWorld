73c45a2f3d0a5fef586d401c5cf3fa7c00622fcd
Saah <sarahvivloos@gmail.com>
2024-05-21 10:20:36 -0300

    Added Floo Flame Network

---
+++ [CP] Hogwarts/assets/Maps/FlooFlames..tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="z_FlooFlames" tilewidth="16" tileheight="16" tilecount="50" columns="10">
+ <image source="FlooFlames.png" width="160" height="80"/>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="175"/>
+   <frame tileid="2" duration="175"/>
+   <frame tileid="3" duration="175"/>
+   <frame tileid="4" duration="175"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="175"/>
+   <frame tileid="12" duration="175"/>
+   <frame tileid="13" duration="175"/>
+   <frame tileid="14" duration="175"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <animation>
+   <frame tileid="30" duration="175"/>
+   <frame tileid="32" duration="175"/>
+   <frame tileid="34" duration="175"/>
+   <frame tileid="36" duration="175"/>
+  </animation>
+ </tile>
+ <tile id="31">
+  <animation>
+   <frame tileid="31" duration="175"/>
+   <frame tileid="33" duration="175"/>
+   <frame tileid="35" duration="175"/>
+   <frame tileid="37" duration="175"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="175"/>
+   <frame tileid="42" duration="175"/>
+   <frame tileid="44" duration="175"/>
+   <frame tileid="46" duration="175"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="41" duration="175"/>
+   <frame tileid="43" duration="175"/>
+   <frame tileid="45" duration="175"/>
+   <frame tileid="47" duration="175"/>
+  </animation>
+ </tile>
+</tileset>
